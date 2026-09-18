0a851b360f05c26f85b29e2b06fa4551d7cf7c4e
iarlqsensi <rwp7hvpgzd@privaterelay.appleid.com>
2026-09-18 07:03:47 -0300

    Update discord.js

--- api/auth/discord.js
+++ api/auth/discord.js
@@ -1,1 +1,17 @@
 
+export default function handler(req, res) {
+  const clientId = process.env.DISCORD_CLIENT_ID;
+  const redirectUri = process.env.DISCORD_REDIRECT_URI;
+
+  const params = new URLSearchParams({
+    client_id: clientId,
+    response_type: "code",
+    redirect_uri: redirectUri,
+    scope: "identify"
+  });
+
+  res.redirect(
+    302,
+    `https://discord.com/oauth2/authorize?${params.toString()}`
+  );
+}
